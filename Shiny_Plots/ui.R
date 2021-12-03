8678fc558933a9613c57472099e5304efa288ee3
sergiopalmat <palma.sergio@gmail.com>
2021-12-03 07:37:46 -0600

    Update ui.R

--- Shiny_Plots/ui.R
+++ Shiny_Plots/ui.R
@@ -1,27 +1,16 @@
 library(shiny)
-library(DT)
 
 shinyUI(fluidPage(
-  titlePanel('Tarea Shiny Plots'),
-  tabsetPanel(
-    column(8,
-           h2('Plots Interactivos'),
-           fluidRow(
-             plotOutput('plot2',
-                        click = 'clk',
-                        dblclick = 'dclk',
-                        hover = 'mouse_hover',
-                        brush = 'mouse_brush'
-             ),
-             
-             #para limpiar el rango
-             actionButton("limpiar_grph", "Limpiar"),
-           )
-    ),
-    column(8,
-           h2('Tabla DT'),
-           DT::dataTableOutput('tabla_dt')
-    )
-  )
-  
+
+   
+    titlePanel("Tarea Shiny Plots"),
+    plotOutput('plot',
+                    click = 'clk', 
+                   dblclick = 'dclk',
+                  hover = 'mouse_hover',
+                 brush = 'mouse_brush'
+     ),
+    DT::dataTableOutput('mtcars_tbl')
+
+
 ))
