282921391c89b837ebf16cf3a69f8006cd4219b1
sergiopalmat <palma.sergio@gmail.com>
2021-12-03 08:09:23 -0600

    Update ui.R

--- Shiny_Plots/ui.R
+++ Shiny_Plots/ui.R
@@ -3,15 +3,12 @@ library(shiny)
 shinyUI(fluidPage(
 
   titlePanel("Shiny Plots"),
-  
-  plotOutput('plot_click_option',
-             click = 'click',
+  plotOutput('plot',
+             click = 'clk', 
              dblclick = 'dclk',
              hover = 'mouse_hover',
              brush = 'mouse_brush'
   ),
-
-  tableOutput('mtcars_tbl_click'),
-  tableOutput('mtcars_tbl'),
-  
+  DT::dataTableOutput('mtcars_tbl')
+ 
 ))
