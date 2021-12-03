08beb0d825334ae7cde7df7ff147dfcb103ae209
sergiopalmat <palma.sergio@gmail.com>
2021-12-03 07:37:10 -0600

    Update server.R

--- Shiny_Plots/server.R
+++ Shiny_Plots/server.R
@@ -1,73 +1,57 @@
 library(shiny)
-library(ggplot2)
 library(dplyr)
+library(ggplot2)
 
-shinyServer(function(input, output) {
-    
+shinyServer(function(input, output, session) {
 
-    vals = reactiveValues(
-        keeprows = rep(TRUE, nrow(mtcars))
-    )
-    
-    vals2 = reactiveValues(
-        keeprows2 = rep(TRUE, nrow(mtcars))
-    )
-    
-    vals3 = reactiveValues(
-        keeprows3 = rep(TRUE, nrow(mtcars))
-    )
-    
-    vals4 = reactiveValues(
-        keeprows4 = rep(TRUE, nrow(mtcars))
-    )
-    
-    output$plot2 = renderPlot({
-        keep = mtcars[vals$keeprows, , drop = FALSE]
-        exclude = mtcars[!vals$keeprows, , drop = FALSE]
-        exclude_clk = mtcars[!vals2$keeprows2, , drop = FALSE]
-        exclude_dclk = mtcars[!vals3$keeprows3, , drop = FALSE]
-        exclude_brush = mtcars[!vals4$keeprows4, , drop = FALSE]
-        
-        ggplot(keep, aes(wt, mpg)) + geom_point(size = 8, color="black") + 
-            geom_point(data = exclude, shape = 21, color = "gray", fill="gray", size = 8)+
-            geom_point(data = exclude_clk, shape = 21, color = "green", fill="green", size = 8)+
-            geom_point(data = exclude_dclk, shape = 21, color = "black", fill="black", size = 8)+
-            geom_point(data = exclude_brush, shape = 21, color = "green", fill="green", size = 8)+
-            theme_classic()
-    })
-    
-    output$tabla_dt = DT::renderDataTable({mtcars})
-    
-    observeEvent(input$mouse_hover,{
-        res_hover = nearPoints(mtcars, input$mouse_hover, allRows = TRUE, maxpoints = 1)
-        vals$keeprows = xor(vals$keeprows, res_hover$selected_)
-    })
-    
-    observeEvent(input$clk,{
-        res_clk = nearPoints(mtcars, input$clk, allRows = TRUE, maxpoints = 1)
-        res_clk_tb = nearPoints(mtcars, input$clk, maxpoints = 1)
-        vals2$keeprows2 = xor(vals2$keeprows2, res_clk$selected_)
-        output$tabla_dt = DT::renderDataTable({res_clk_tb})
-    })
-    
-    observeEvent(input$dclk,{
-        res_dclk = nearPoints(mtcars, input$dclk, allRows = TRUE, maxpoints = 1)
-        vals3$keeprows3 = xor(vals3$keeprows3, res_dclk$selected_)
-    })
-    
-    observeEvent(input$mouse_brush,{
-        res_brush = brushedPoints(mtcars, input$mouse_brush, allRows = TRUE)
-        res_brush_tbl = brushedPoints(mtcars, input$mouse_brush)
-        vals4$keeprows4 = xor(vals4$keeprows4, res_brush$selected_)
-        output$tabla_dt = DT::renderDataTable({res_brush_tbl })
-    })
+  selected_points <- reactiveVal(rep(FALSE, nrow(mtcars)))
+  
+  
+  observeEvent(input$mouse_brush, {
+    selected <- brushedPoints(mtcars, input$mouse_brush, allRows=TRUE)['selected_']
+    selected_points(selected | selected_points())
+  })
+  
+  observeEvent(input$clk, {
+    selected <- nearPoints(mtcars, input$clk, xvar="wt", yvar="mpg", allRows=TRUE)['selected_']
+    selected_points(selected | selected_points())
+  })
+  
+  observeEvent(input$mouse_hover, {
+    selected <- nearPoints(mtcars, input$mouse_hover, xvar="wt", yvar="mpg", allRows=TRUE)['selected_']
+    selected_points(selected_points() & xor(selected_points(), selected))
+  })
+  
+  observeEvent(input$dclk, {
+    selected_points(rep(FALSE, nrow(mtcars)))
+  })
 
-    observeEvent(input$limpiar_grph, {
-        vals$keeprows <- rep(TRUE, nrow(mtcars))
-        vals2$keeprows2 <- rep(TRUE, nrow(mtcars))
-        vals3$keeprows3 <- rep(TRUE, nrow(mtcars))
-        vals4$keeprows4 <- rep(TRUE, nrow(mtcars))
-        output$tabla_dt = DT::renderDataTable({mtcars})
-    })
+  
+  output$plot <- renderPlot({
+    mtcars$selected <- selected_points()
+    ggplot(mtcars, aes(wt, mpg)) + 
+      geom_point(aes(colour = selected)) +
+      scale_color_manual(values=c("TRUE"="green","FALSE"="gray")) +
+    theme(panel.border = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_rect(fill = "white",
+                                         colour = "black"),
+          legend.position = "none")
+    
+  }, res = 150)
+  
+  
+  output$mtcars_tbl <- DT::renderDataTable({
+    df <- mtcars
+    if(any(selected_points())){ 
+      df$selected <- selected_points()
+      df <- df %>% 
+              filter(selected == TRUE) %>%
+              select(-selected)
+    } 
+    df
+  })
+  
 
 })
